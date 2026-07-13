cc67134c5aea0e042f29e587a5f7aae9089847fe
Joshua Lobo <47839761+JoshuaLobo@users.noreply.github.com>
2026-07-13 13:24:16 +0530

    Update firebaseService.js

--- admin/firebaseService.js
+++ admin/firebaseService.js
@@ -1,11 +1,19 @@
+import {db} from "../firebase.js";
+
+import {CONFIG} from "./config.js";
+
 export async function loadFeedback(){
 
 }
 
-export async function updateStatus(){
+export async function updateStatus(id,status){
 
 }
 
-export async function deleteFeedback(){
+export async function deleteFeedback(id){
+
+}
+
+export async function getStatistics(){
 
 }
