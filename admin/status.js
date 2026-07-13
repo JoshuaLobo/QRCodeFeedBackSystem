14654bbe33bd466af06b883731cf0d4448fad46a
Joshua Lobo <47839761+JoshuaLobo@users.noreply.github.com>
2026-07-13 13:35:28 +0530

    Update status.js

--- admin/status.js
+++ admin/status.js
@@ -1,3 +1,38 @@
-export async function toggleRead(item){
+// =====================================
+// Status Module
+// =====================================
+
+import { CONFIG } from "./config.js";
+
+import { updateFeedbackStatus } from "./firebaseService.js";
+
+/**
+ * Toggle Read / Unread
+ * @param {Object} item
+ */
+
+export async function toggleStatus(item){
+
+    const newStatus =
+
+        item.status === CONFIG.STATUS.READ
+
+        ?
+
+        CONFIG.STATUS.UNREAD
+
+        :
+
+        CONFIG.STATUS.READ;
+
+    await updateFeedbackStatus(
+
+        item.id,
+
+        newStatus
+
+    );
+
+    item.status = newStatus;
 
 }
